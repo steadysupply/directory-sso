00dca96d26760abadbca03e00b6ed6a1654c098c
Rich Tier <rikatee@gmail.com>
2016-11-02 22:32:35 +0000

    Add --noinput to migrate

--- docker/cmd-webserver.sh
+++ docker/cmd-webserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -xe
 
-python /usr/src/app/manage.py migrate
+python /usr/src/app/manage.py migrate --noinput
 mkdir -p /usr/src/app/sso/static
 python /usr/src/app/manage.py collectstatic --noinput
 gunicorn config.wsgi --bind 0.0.0.0:$PORT --log-file -
